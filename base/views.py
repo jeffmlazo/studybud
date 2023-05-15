f9193885327ce5b773f935ee8869068805a8a0ed
Jeff Ray <jeffmlazo@yahoo.com>
2023-05-16 02:21:22 +0800

    Add theme layouts for login page, register page, activity sections & topic sections Add Django REST framework Add getRoutes & getRooms api

--- base/views.py
+++ base/views.py
@@ -75,7 +75,8 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    # [0:5] index from 0 to 5 queries returned only
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     # Browse topics with filtering features
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
@@ -199,3 +200,14 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form': form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
